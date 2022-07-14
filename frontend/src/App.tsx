7b97f97a5e969152c9b6b2559e33c56845b16db5
Samuel Fernandes <samu.17.31@hotmail.com>
2022-07-14 15:34:50 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
-import "./index.css"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import "./index.css";
 
 function App() {
   return (
@@ -17,4 +17,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
